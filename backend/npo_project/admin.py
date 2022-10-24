c2e38c82a231f1c1aedb6ceb8f2c4faab3e21839
Sergey Krapivnitskiy <ya.uplink@yandex.ru>
2022-10-24 13:53:14 +0300

    Создана модель Partners и Principles
    * Модель Partners - Наши партнеры
    * Модель Principles - Наши принципы
    * Подключено управление моделей в админке
    * Включена статика при DEBUG = True
    * Добавлена media в .gitignore
    * Обновлен requirements:
    Pillow==9.2.0 и sorl-thumbnail==12.9.0

--- backend/npo_project/admin.py
+++ backend/npo_project/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import Partners, Principles
+
+
+@admin.register(Principles)
+class PrinciplesAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'comment',
+    )
+    search_fields = ('title',)
+    list_filter = ('title',)    
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Partners)
+class PartnersAdmin(admin.ModelAdmin):
+    fields = ('name', 'text', 'image', 'logotype', 'url')
+    list_display = (
+        'name',
+        'text',
+        'logotype',
+        'url',
+    )
+    list_editable = ('text',)
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+    readonly_fields = ('logotype',)
+
+    def logotype(self, obj):
+        return mark_safe(
+            f'<img src="{obj.image.url}" style="max-height: 100px;">')
--- backend/npo_project/models.py
+++ backend/npo_project/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+
+class Partners(models.Model):
+    """Наши партнеры"""
+
+    name = models.CharField(
+                        max_length=100,
+                        unique=True,
+                        blank=False,
+                        verbose_name='Название партнера')
+    text = models.TextField(
+                        blank=False,
+                        unique=True,
+                        verbose_name='Краткая информация о партнере')
+    image = models.ImageField(
+                        blank=False,
+                        upload_to='partners_img/',
+                        verbose_name='Логотип партнера')
+    url = models.URLField(
+                        blank=False,
+                        verbose_name='Ссылка на сайт партнера')
+
+    class Meta:
+        verbose_name = 'Партнера'
+        verbose_name_plural = 'Наши партнеры'
+        constraints = [
+            models.UniqueConstraint(fields=['name', 'url'],
+                                    name='unique_partners')
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+class Principles(models.Model):
+    """Наши принципы"""
+
+    title = models.CharField(
+                        max_length=350,
+                        blank=False,
+                        unique=True,
+                        verbose_name='Описание принципа')
+    comment = models.TextField(
+                        blank=True,
+                        verbose_name='Дополнение к принципу')
+
+    class Meta:
+        verbose_name = 'Принцип'
+        verbose_name_plural = 'Наши принципы'
+
+    def __str__(self):
+        return self.title
