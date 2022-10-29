0513f0e51113f7d046976fa4353c66bf9ec900e9
Сергей <bazzz@list.ru>
2022-10-29 13:05:50 +0300

    FAQ_Location

--- backend/npo_project/admin.py
+++ backend/npo_project/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import FAQ, Location
 
-# Register your models here.
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ('question', 'answer') 
+    search_fields = ('question',)
+
+admin.site.register(FAQ, FAQAdmin)
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('adress', 'image') 
+    search_fields = ('adress',) 
+
+admin.site.register(Location, LocationAdmin)
--- backend/npo_project/models.py
+++ backend/npo_project/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class FAQ(models.Model):
+    question = models.TextField(
+                            verbose_name='Вопрос',
+                            max_length=50)
+    answer = models.TextField(
+                            verbose_name='Ответ',
+                            max_length=50)
+    class Meta:
+        verbose_name = 'Вопросы'
+        verbose_name_plural = 'Часто задаваемые вопросы'
+
+    def __str__(self):
+        return (f'{self.question}, '
+                f'{self.answer}')    
+
+class Location(models.Model):
+    adress = models.TextField(
+                            verbose_name='Адрес',
+                            max_length=50)
+    image = models.ImageField(
+                        blank=False,
+                        upload_to='location_img/',
+                        verbose_name='Карта')
+    class Meta:
+        verbose_name = 'Адрес'
+        verbose_name_plural = 'Адреса'
+
+    def __str__(self):
+        return self.adress
