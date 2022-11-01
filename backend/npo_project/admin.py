3c72aca1b5b7b41f216f7de92ccfbec89da765c0
Сергей <bazzz@list.ru>
2022-11-01 11:33:25 +0300

    decorator_for_admin

--- backend/npo_project/admin.py
+++ backend/npo_project/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from .models import FAQ, Location
 
+
+@admin.register(FAQ)
 class FAQAdmin(admin.ModelAdmin):
-    list_display = ('question', 'answer') 
+    list_display = ('question', 'answer')
     search_fields = ('question',)
 
-admin.site.register(FAQ, FAQAdmin)
-
+@admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ('adress', 'image') 
-    search_fields = ('adress',) 
-
-admin.site.register(Location, LocationAdmin)
+    list_display = ('adress', 'image')
+    search_fields = ('adress',)
--- backend/npo_project/models.py
+++ backend/npo_project/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class FAQ(models.Model):
     question = models.TextField(
                             verbose_name='Вопрос',
@@ -10,13 +11,15 @@ class FAQ(models.Model):
     answer = models.TextField(
                             verbose_name='Ответ',
                             max_length=50)
+
     class Meta:
         verbose_name = 'Вопросы'
         verbose_name_plural = 'Часто задаваемые вопросы'
 
     def __str__(self):
         return (f'{self.question}, '
-                f'{self.answer}')    
+                f'{self.answer}')
+
 
 class Location(models.Model):
     adress = models.TextField(
@@ -26,6 +29,7 @@ class Location(models.Model):
                         blank=False,
                         upload_to='location_img/',
                         verbose_name='Карта')
+
     class Meta:
         verbose_name = 'Адрес'
         verbose_name_plural = 'Адреса'
