8de4a9d9d496989abaea8a3aea0c0b8a95e585cb
Леонид <lieonid2003@mail.ru>
2022-11-10 18:37:02 +0300

    files fix

--- backend/npo_project/admin.py
+++ backend/npo_project/admin.py
@@ -7,21 +7,6 @@ from .models import (Course, Tag, Partners, Principles, Requisites)
 admin.AdminSite.site_header = 'Администрирование - Балапанлар'
 
 
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    search_fields = ('name',)
-
-
-@admin.register(Course)
-class CourseAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'category', 'age_groups',
-                    'duration', 'description', 'skills')
-    search_fields = ('name',)
-    list_editable = ('category', 'age_groups',
-                     'duration', 'description', 'skills')
-
-
 @admin.register(Principles)
 class PrinciplesAdmin(admin.ModelAdmin):
     fields = ('text', 'image', 'picture')
---
+++ backend/npo_project/admin/course_tag.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+from ..models import Course, Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'age_groups',
+                    'duration', 'description', 'skills')
+    search_fields = ('name',)
+    list_editable = ('category', 'age_groups',)
