4753bfcdf90d589fae766adc801f3afad040bf2b
simatheone <samone07@mail.ru>
2022-11-13 21:41:01 +0300

    add imports in __init__ files to __all__ variable (admin, model)

--- backend/npo_project/admin/__init__.py
+++ backend/npo_project/admin/__init__.py
@@ -1,12 +1,29 @@
-from .course_price_admin import (  # noqa
-    CoursePriceAdmin, IncludedInCoursePriceAdmin,
+from .course_price_admin import (
+    CoursePriceAdmin,
+    IncludedInCoursePriceAdmin,
     NotIncludedInCoursePriceAdmin,
 )
-from .course_tag_admin import CourseAdmin, TagAdmin  # noqa
-from .faq_admin import FAQAdmin  # noqa
-from .location_admin import LocationAdmin  # noqa
-from .partners_admin import PartnersAdmin  # noqa
-from .principles_admin import PrinciplesAdmin  # noqa
-from .program_admin import ProgramAdmin, ProgramCharacteristicAdmin  # noqa
-from .requisites_admin import RequisitesAdmin  # noqa
-from .user_admin import CustomUserAdmin  # noqa
+from .course_tag_admin import CourseAdmin, TagAdmin
+from .faq_admin import FAQAdmin
+from .location_admin import LocationAdmin
+from .partners_admin import PartnersAdmin
+from .principles_admin import PrinciplesAdmin
+from .program_admin import ProgramAdmin, ProgramCharacteristicAdmin
+from .requisites_admin import RequisitesAdmin
+from .user_admin import CustomUserAdmin
+
+__all__ = [
+    'CourseAdmin',
+    'CoursePriceAdmin',
+    'CustomUserAdmin',
+    'FAQAdmin',
+    'IncludedInCoursePriceAdmin',
+    'LocationAdmin',
+    'NotIncludedInCoursePriceAdmin',
+    'PartnersAdmin',
+    'PrinciplesAdmin',
+    'ProgramAdmin',
+    'ProgramCharacteristicAdmin',
+    'RequisitesAdmin',
+    'TagAdmin',
+]
--- backend/npo_project/models/__init__.py
+++ backend/npo_project/models/__init__.py
@@ -1,13 +1,33 @@
-from .benefit import Benefit  # noqa
-from .cource_tags import Course, Tag  # noqa
-from .course_price import (  # noqa
-    CoursePrice, IncludedInCoursePrice, NotIncludedInCoursePrice,
+from .benefit import Benefit
+from .cource_tags import Course, Tag
+from .course_price import (
+    CoursePrice,
+    IncludedInCoursePrice,
+    NotIncludedInCoursePrice,
 )
-from .faq import FAQ  # noqa
-from .location import Location  # noqa
-from .partners import Partners  # noqa
-from .principles import Principles  # noqa
-from .program import Program, ProgramCharacteristic  # noqa
-from .requisites import Requisites  # noqa
-from .team_member import TeamMember  # noqa
-from .user import CustomUser  # noqa
+from .faq import FAQ
+from .location import Location
+from .partners import Partners
+from .principles import Principles
+from .program import Program, ProgramCharacteristic
+from .requisites import Requisites
+from .team_member import TeamMember
+from .user import CustomUser
+
+__all__ = [
+    'Benefit',
+    'Course',
+    'CoursePrice',
+    'CustomUser',
+    'FAQ',
+    'IncludedInCoursePrice',
+    'Location',
+    'NotIncludedInCoursePrice',
+    'Partners',
+    'Principles',
+    'Program',
+    'ProgramCharacteristic',
+    'Requisites',
+    'Tag',
+    'TeamMember',
+]
