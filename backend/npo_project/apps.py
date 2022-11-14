6eeee8707a34b35fad5435de91b8a943a517a309
Татьяна Овчинникова <tanja-ovc@yandex.ru>
2022-11-14 04:08:45 +0300

    BigAutoField instead of AutoField

--- backend/npo_project/apps.py
+++ backend/npo_project/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 
 
 class NpoProjectConfig(AppConfig):
-    default_auto_field = 'django.db.models.AutoField'
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'npo_project'
     verbose_name = 'Управление проектом'
