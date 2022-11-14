90b6ab4e395a67aa7b79a75fe85728fc4e629963
Татьяна Овчинникова <tanja-ovc@yandex.ru>
2022-11-14 04:13:06 +0300

    adding __all__ variable

--- backend/npo_project/admin/__init__.py
+++ backend/npo_project/admin/__init__.py
@@ -1,8 +1,19 @@
-from .course_price_admin import (  # noqa
+from .course_price_admin import (
     CoursePriceAdmin, IncludedInCoursePriceAdmin,
     NotIncludedInCoursePriceAdmin,
 )
-from .program_admin import ProgramAdmin, ProgramCharacteristicAdmin  # noqa
-from .user_admin import CustomUserAdmin  # noqa
-from .FAQ_admin import FAQAdmin # noqa
-from .Location_admin import LocationAdmin # noqa
+from .program_admin import ProgramAdmin, ProgramCharacteristicAdmin
+from .user_admin import CustomUserAdmin
+from .FAQ_admin import FAQAdmin
+from .Location_admin import LocationAdmin
+
+__all__ = [
+    CoursePriceAdmin,
+    CustomUserAdmin,
+    FAQAdmin,
+    IncludedInCoursePriceAdmin,
+    LocationAdmin,
+    NotIncludedInCoursePriceAdmin,
+    ProgramAdmin,
+    ProgramCharacteristicAdmin,
+]
