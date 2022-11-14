6e5c1f8c666b3d688bb2f42c2e6eacd5d680f0f6
Татьяна Овчинникова <tanja-ovc@yandex.ru>
2022-11-14 14:57:14 +0300

    flake8 amendments

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -23,7 +23,8 @@ router_v1.register('requisites', RequisitesViewSet, basename='requisites')
 router_v1.register('programs', ProgramViewSet, basename='programs')
 router_v1.register('programs_characteristics', ProgramCharacteristicViewSet,
                    basename='programs_characteristics')
-router_v1.register('course_prices', CoursePriceViewSet, basename='course_prices')
+router_v1.register('course_prices', CoursePriceViewSet,
+                   basename='course_prices')
 router_v1.register('included_in_course_price', IncludedInCoursePriceViewSet,
                    basename='included_in_course_price')
 router_v1.register('not_included_in_course_price',
