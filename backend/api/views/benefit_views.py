328d7c2724e925b66aba6161e55fb0916d6899fd
photometer <androsovaelizaveta@mail.ru>
2022-11-21 15:57:45 +0300

    Swagger and redoc are added

--- backend/api/views/benefit_views.py
+++ backend/api/views/benefit_views.py
@@ -1,6 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
 
 from npo_project.models import Benefit
 from ..filters import BenefitFilter
@@ -9,21 +8,10 @@ from ..serializers import BenefitRoleSerializer, BenefitSerializer
 
 class BenefitViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Benefit.objects.all()
+    filter_backends = (DjangoFilterBackend,)
     filterset_class = BenefitFilter
 
     def get_serializer_class(self):
-        if 'for_' in self.request.get_full_path():
+        if not self.request or self.request.query_params:
             return BenefitRoleSerializer
         return BenefitSerializer
-
-    @action(methods=["get", ], detail=False)
-    def for_children(self, request):
-        beneficial_to = self.queryset.filter(beneficial_to='CHILD')
-        serializer = self.get_serializer(beneficial_to, many=True)
-        return Response(serializer.data)
-
-    @action(methods=["get", ], detail=False)
-    def for_parents(self, request):
-        beneficial_to = self.queryset.filter(beneficial_to='PARENT')
-        serializer = self.get_serializer(beneficial_to, many=True)
-        return Response(serializer.data)
--- backend/balapanlar/urls.py
+++ backend/balapanlar/urls.py
@@ -1,11 +1,43 @@
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.urls import include, path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Balapanlar API",
+        default_version='v1',
+        description="Документация для приложения api проекта Балапанлар",
+        # terms_of_service="URL страницы с пользовательским соглашением",
+        # contact=openapi.Contact(email="?"),
+        # license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls', namespace='api'))
+    path('api/', include('api.urls', namespace='api')),
+    url(
+        r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json'
+    ),
+    url(
+        r'^swagger/$',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+    ),
+    url(
+        r'^docs/$',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'
+    ),
 ]
 
 if settings.DEBUG:
