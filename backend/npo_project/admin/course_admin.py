707fa61b36c44cdc164ad2b0f99487cc9d9aca1f
Татьяна Овчинникова <tanja-ovc@yandex.ru>
2022-12-04 00:06:00 +0300

    CourseCategory model made visible on admin site

--- backend/npo_project/admin/course_admin.py
+++ backend/npo_project/admin/course_admin.py
@@ -11,25 +11,9 @@ from django.contrib import admin
 from ..models import Course, CourseCategory, CourseTag
 
 
-class HiddenAdmin(admin.ModelAdmin):
-    """
-    Inheriting from this model when creating an admin model
-    allows hiding the admin model from the list on the admin page
-    while keeping it registered there.
-    This makes the models list on the admin page clearer but keeps
-    the functionality for the related models, which otherwise (without
-    registering the "hidden" model at all) would be lost.
-    """
-    def get_model_perms(self, request):
-        """
-        Return empty perms dict thus hiding the model from admin index.
-        """
-        return {}
-
-
 @admin.register(CourseCategory)
-class CourseCategoryAdmin(HiddenAdmin):
-    pass
+class CourseCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
 
 
 @admin.register(CourseTag)
