51f7a0b1c73dbf3f519bbc43c07a5e545777a5ee
Татьяна Овчинникова <tanja-ovc@yandex.ru>
2023-01-06 02:49:56 +0300

    adding image preview to Program model

--- backend/npo_project/admin/program_admin.py
+++ backend/npo_project/admin/program_admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from ..models import Program, ProgramCharacteristic
 
 
 @admin.register(Program)
 class ProgramAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'location', 'image',)
+    list_display = ('name', 'description', 'location', 'image',
+                    'image_preview')
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        if obj.image != '':
+            return mark_safe(
+                f'<img src="{obj.image.url}" style="max-height: 100px;">')
+        return 'Картинка ещё не сохранена'
+    image_preview.short_description = 'Предпросмотр картинки'
 
 
 @admin.register(ProgramCharacteristic)
